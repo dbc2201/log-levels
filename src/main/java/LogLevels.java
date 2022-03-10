99ec0d832d71bdc65fca03a4b43315afc160a15b
dbc2201 <divyanshb22@gmail.com>
2022-03-10 09:18:49 +0530

    complete the message() method

--- src/main/java/LogLevels.java
+++ src/main/java/LogLevels.java
@@ -1,7 +1,7 @@
 public class LogLevels {
 
     public static String message(String logLine) {
-        throw new UnsupportedOperationException("Please implement the (static) LogLine.message() method");
+        return logLine.split(":")[1].trim();
     }
 
     public static String logLevel(String logLine) {
